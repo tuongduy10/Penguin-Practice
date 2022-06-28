a03fc87627aa55cbdbefb3ea80ac5e0842db6e2b
Tường Duy Võ <duy.vo@bstarsolutions.com>
2022-06-28 17:52:11 +0700

    get notification with virtual scroll

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -1,18 +1,26 @@
 import { NotificationPageRequest } from './dtos/notification.request';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  notifications$ = new BehaviorSubject<any>(null);
 
   constructor(private httpClient: HttpClient) { }
 
+  setNotification(response: any){
+    this.notifications$.next(response);
+  }
   getNotification(request: NotificationPageRequest){
     return this.httpClient.post("/tw/notification-service/notification/", request);
   }
   getUnread(userId: number){
     return this.httpClient.post("/tw/notification-service/notification/read/unread", { userId: userId });
+  }
+  markAllAsRead(request: NotificationPageRequest){
+    return this.httpClient.post("/tw/notification-service/notification/read/mark-all-as-read", request);
   }
 }
--- src/app/public/layouts/main-layout/main-layout.component.ts
+++ src/app/public/layouts/main-layout/main-layout.component.ts
@@ -3,11 +3,13 @@ import { NotificationService } from '../../../core/services/notification/notific
 import { UserProfile } from './../../../modules/auth/dtos/responses/profile.response';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Languages } from 'src/app/core/constant/languanges';
 import { AuthService } from 'src/app/modules/auth/auth.service';
 import { EnumNotification } from 'src/app/core/services/notification/enum/notification.enum';
 import { NotificationModel } from 'src/app/core/services/notification/dtos/notification.model';
+import { CdkVirtualScrollViewport } from "@angular/cdk/scrolling";
+import { concat } from 'lodash';
 
 @Component({
   selector: 'app-main-layout',
@@ -15,12 +17,17 @@ import { NotificationModel } from 'src/app/core/services/notification/dtos/notif
   styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
+  @ViewChild(CdkVirtualScrollViewport) viewport!: CdkVirtualScrollViewport;
+
   isSidebarCollapsed = false;
   isHeaderCollapsed = false;
   languages = Languages;
   selectedLang = this.cookieService.get("lang");
   unread = 0;
   profile!: UserProfile;
+  totalPages!: number;
+  totalElements!: number;
+  pageNumber: number = 1;
   constructor(
     private cookieService: CookieService,
     private translateService: TranslateService,
@@ -48,36 +55,59 @@ export class MainLayoutComponent implements OnInit {
   isAllActive = false;
   getAllNotification(){
     this.notiPageRequest.status = EnumNotification.All;
-    this.notificationService.getNotification(this.notiPageRequest).subscribe((response: any) =>{
-      this.notifications = response.data as NotificationModel[];
-    });
+    this.getNotification();
     this.deActivateNotifiAction();
     this.isAllActive = true;
   }
   isReadActive = false;
   getReadNotification(){
     this.notiPageRequest.status = EnumNotification.Read
-    this.notificationService.getNotification(this.notiPageRequest).subscribe((response: any) =>{
-      this.notifications = response.data as NotificationModel[];
-    });
+    this.getNotification();
     this.deActivateNotifiAction();
     this.isReadActive = true;
   }
   isUnreadActive = false;
   getUnreadNotification(){
     this.notiPageRequest.status = EnumNotification.Unread;
-    this.notificationService.getNotification(this.notiPageRequest).subscribe((response: any) =>{
-      this.notifications = response.data as NotificationModel[];
-    });
+    this.getNotification();
     this.deActivateNotifiAction();
     this.isUnreadActive = true;
+  }
+  getNotification(){
+    this.notificationService.getNotification(this.notiPageRequest).subscribe((response: any) =>{
+      if(response.data){
+        this.notifications = response.data as NotificationModel[];
+        this.notificationService.setNotification(response);
+      }
+      // meta response
+      if(response.meta){
+        this.totalPages = response.meta.totalPages;
+        this.totalElements = response.meta.totalElements;
+        this.pageNumber = response.meta.pageNumber;
+      }
+    });
+  }
+  getNewNotification(currentData: NotificationModel[]){
+    this.notificationService.getNotification(this.notiPageRequest).subscribe((response: any) =>{
+      if(response.data){
+        this.notifications = concat(currentData, response.data);
+      }
+      // meta response
+      if(response.meta){
+        this.totalPages = response.meta.totalPages;
+        this.totalElements = response.meta.totalElements;
+        this.pageNumber = response.meta.pageNumber;
+      }
+    });
   }
   getUnread(id: number){
     this.notificationService.getUnread(id).subscribe((response: any) => {
       this.unread = response.data as number;
+      this.notificationService.setNotification(response);
     });
   }
   deActivateNotifiAction(){
+    this.notiPageRequest.pageNumber = 1;
     this.isAllActive = false;
     this.isUnreadActive = false;
     this.isReadActive = false;
@@ -89,5 +119,51 @@ export class MainLayoutComponent implements OnInit {
   }
   logout(){
     this.authService.logout();
+  }
+  scrolledIndexChange(){
+    if(this.pageNumber == this.totalPages){
+      return;
+    }
+    const end = this.viewport.getRenderedRange().end;
+    const total = this.viewport.getDataLength();
+    if(end === total){
+      this.getNotificationByScrolled();
+      return;
+    }
+  }
+  getNotificationByScrolled(){
+    if(this.isAllActive){
+      this.notiPageRequest.status = EnumNotification.All;
+    }
+    if(this.isReadActive){
+      this.notiPageRequest.status = EnumNotification.Read;
+    }
+    if(this.isUnreadActive){
+      this.notiPageRequest.status = EnumNotification.Unread;
+    }
+
+    this.pageNumber = this.pageNumber + 1;
+    this.notiPageRequest.pageNumber = this.pageNumber;
+    this.getNewNotification(this.notifications);
+  }
+  trackByIdx(i: number){
+    return i;
+  }
+  markAllAsRead(){
+    this.notificationService.markAllAsRead(this.notiPageRequest).subscribe((response: any) => {
+      if(response.data){
+        this.notifications = response.data as NotificationModel[];
+        this.notificationService.setNotification(response);
+      }
+      // meta response
+      if(response.meta){
+        this.totalPages = response.meta.totalPages;
+        this.totalElements = response.meta.totalElements;
+        this.pageNumber = response.meta.pageNumber;
+      }
+    });
+  }
+  refreshNotification(){
+
   }
 }
